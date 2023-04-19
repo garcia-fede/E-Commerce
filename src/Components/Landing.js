4eb643c23ee822d4d513d3a1bde1a74f2823810c
Federico Garcia <federicoignaciogarcia@gmail.com>
2023-04-19 01:58:00 -0300

    Working Filter - Color disabled

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -5,28 +5,35 @@ import { db } from "./firebaseConfig"
 import { collection,getDocs} from "firebase/firestore"
 const Landing = () => {
 
-    let [products,setProducts] = useState([])
-    let [addEvent,setAddEvent] = useState(true)
-    let [showProducts,setShowProducts] = useState([])
-    let [clothesFilter,setClothesFilter] = useState([])
-    let [genderFilter,setGenderFilter] = useState([])
-    let [colorFilter,setColorFilter] = useState([])
+    const [products,setProducts] = useState([])
+    const [showProducts,setShowProducts] = useState([])
+    const [addEvent,setAddEvent] = useState(true)
+    const [clothesFilter,setClothesFilter] = useState([])
+    const [genderFilter,setGenderFilter] = useState([])
+    const [colorFilter,setColorFilter] = useState([])
 
-    function updateProducts() {
-        console.log(clothesFilter,genderFilter)
+    const updateProducts = ()=>{
         let clothes = clothesFilter.length
         let gender = genderFilter.length
         let color = colorFilter.length
-        // if(clothes==0&&gender==0&&color==0){
-        //     setShowProducts(products)
-        // }
-        // else{
-        //     let filteredProducts = products.filter(product=>clothesFilter.includes(product.category)&&genderFilter.includes(product.gender))
-        //     setShowProducts(filteredProducts)
-        // }
+        if(clothes==0&&gender==0&&color==0){
+            setShowProducts(products)
+        }
+        else if(clothes>0&&gender==0&&color==0){
+            let filteredProducts = products.filter(product=>clothesFilter.includes(product.category))
+            setShowProducts(filteredProducts)
+        }
+        else if(gender>0&&clothes==0&&color==0){
+            let filteredProducts = products.filter(product=>genderFilter.includes(product.gender))
+            setShowProducts(filteredProducts)
+        }
+        else{
+            let filteredProducts = products.filter(product=>clothesFilter.includes(product.category)&&genderFilter.includes(product.gender))
+            setShowProducts(filteredProducts)
+        }
     }
 
-    function addFilter (inputValue,category) {
+    const addFilter = (inputValue,category)=>{
         let addToFilter
         switch(category){
             case 'Clothes':
@@ -54,7 +61,7 @@ const Landing = () => {
         updateProducts()
     }
 
-    function removeFilter (inputValue,category) {
+    const removeFilter = (inputValue,category)=>{
         let removeFromFilter
         let index;
         switch(category){
@@ -85,7 +92,8 @@ const Landing = () => {
         updateProducts()
     }
 
-    function categoryCheck (input) {
+    //Receives the targeted input and extracts it's name and value
+    const categoryCheck = (input)=>{
         let category = input.getAttribute("name")
         let inputValue = input.getAttribute("value")
         switch(category){
@@ -107,19 +115,10 @@ const Landing = () => {
         }
     }
 
-    function filterArrays (input) {
-        if(addEvent){
-            input.addEventListener("change",()=>{
-                categoryCheck(input)
-            })
-            setAddEvent(false)
-        } 
-    }
-
     useEffect(()=>{
         const productsCollection = collection(db,"products")
-        const products = getDocs(productsCollection)
-        products.then((res)=>{
+        const productsUnformatted = getDocs(productsCollection)
+        productsUnformatted.then((res)=>{
             const productsDB = res.docs.map((product)=>{
                 return product.data()
             })
@@ -128,10 +127,7 @@ const Landing = () => {
         }).catch((err)=>{
             console.log(err)
         })
-        const filterInputs = document.querySelectorAll(".filterInput")
-        filterInputs.forEach(input=>filterArrays(input))
     },[])
-    
 
     return (
         <>
@@ -150,19 +146,19 @@ const Landing = () => {
                             </svg>
                         </h2>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" id='T-Shirts' name='Clothes' value='T-Shirts' />
+                            <input className="filterInput" type="checkbox" id='T-Shirts' name='Clothes' value='T-Shirts' onClick={(e) => categoryCheck(e.target)}/>
                             <label htmlFor="T-Shirts">T-Shirts</label>
                         </div>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" id='Jackets' name='Clothes' value='Jackets' />
+                            <input className="filterInput" type="checkbox" id='Jackets' name='Clothes' value='Jackets' onClick={(e) => categoryCheck(e.target)}/>
                             <label htmlFor="Jackets">Jackets</label>
                         </div>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" id='Pants' name='Clothes' value='Pants' />
+                            <input className="filterInput" type="checkbox" id='Pants' name='Clothes' value='Pants' onClick={(e) => categoryCheck(e.target)}/>
                             <label htmlFor="Pants">Pants</label>
                         </div>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" id='Shoes' name='Clothes' value='Shoes' />
+                            <input className="filterInput" type="checkbox" id='Shoes' name='Clothes' value='Shoes' onClick={(e) => categoryCheck(e.target)}/>
                             <label htmlFor="Shoes">Shoes</label>
                         </div>
                     </div>
@@ -175,11 +171,11 @@ const Landing = () => {
                             </svg>
                         </h2>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" value='female' id='female' name='Gender' />
+                            <input className="filterInput" type="checkbox" value='female' id='female' name='Gender' onClick={(e) => categoryCheck(e.target)} />
                             <label htmlFor="female">Women</label>
                         </div>
                         <div className="option">
-                            <input className="filterInput" type="checkbox" value='male' id='male' name='Gender' />
+                            <input className="filterInput" type="checkbox" value='male' id='male' name='Gender' onClick={(e) => categoryCheck(e.target)} />
                             <label htmlFor="male">Men</label>
                         </div>
                     </div>
@@ -209,7 +205,8 @@ const Landing = () => {
                 </div>
             </div>
         </>
-    )
+    ) 
+
 }
 
 export default Landing
